23284a3b82139f89042c4594006f441870a7d4c1
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 04:19:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,11 +5,11 @@
 class Anagram
 
   def initialize(str)
-    @word = str
+    @word = str.chars.sort.join
   end
   def match(arr)
     arr.select do |poss|
-
+      arr.chars.sort.join == @word
     end
   end
 end
