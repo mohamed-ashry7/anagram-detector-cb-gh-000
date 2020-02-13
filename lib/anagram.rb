091d1b9bf9b046a427bdfd67862a4a8a69065e8b
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 04:20:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+
+
+class Anagram
+
+  def initialize(str)
+    @word = str.chars.sort.join
+  end
+  def match(arr)
+    arr.select do |poss|
+      poss.chars.sort.join == @word
+    end
+  end
+end
